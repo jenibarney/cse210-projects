44463ae0373659c15e77b9581eb591adaac4adb6
Jeni Barney <jeniwilliams@byui.edu>
2024-01-20 18:01:59 -0700

    Initial Commit

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+
+        string userName = PromptUserName();
+        int userNumber = PromptUserNumber();
+
+        int squareNumber = SquareNumber(userNumber);
+
+        DisplayResults(userName, squareNumber);
     }
-}
+
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the Program!");
+        }
+
+        static string PromptUserName()
+        {
+            Console.Write("Please enter your name: ");
+            string userName = Console.ReadLine();
+            
+            return userName;
+        }
+
+        static int PromptUserNumber()
+        {
+            Console.Write("Please enter your favorite number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            return number;
+        }
+        static int SquareNumber(int userNumber)
+        {
+            int squareNumber = userNumber * userNumber;
+
+            return squareNumber;
+        }
+        static void DisplayResults(string userName, int squareNumber)
+        {
+            Console.WriteLine($"{userName}, the square of your number is {squareNumber}");
+        }
+    }
