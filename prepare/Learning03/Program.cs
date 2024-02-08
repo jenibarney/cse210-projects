129037d531950eed74e40f994a582c9024c6e7d1
Jeni Barney <jeniwilliams@byui.edu>
2024-02-07 23:50:06 -0700

    Intial Commit

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction f1 = new Fraction();
+        string f1String = f1.MakeStringFraction();
+        Console.WriteLine(f1String);
+        double f1Decimal = f1.CalcDecimal();
+        Console.WriteLine(f1Decimal);
+
+        Fraction f2 = new Fraction(6);
+        string f2String = f2.MakeStringFraction();
+        Console.WriteLine(f2String);
+        double f2Decimal = f2.CalcDecimal();
+        Console.WriteLine(f2Decimal);
+
+        Fraction f3 = new Fraction(4, 7);
+        string f3String = f3.MakeStringFraction();
+        Console.WriteLine(f3String);
+        double f3Decimal = f3.CalcDecimal();
+        Console.WriteLine(f3Decimal);
+        
+
+
+
+        
     }
 }
